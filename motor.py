318f38146ae0dcb68a2541b22a3894aeafd3bb6d
StewardsAcademyRPI <david@thinkingbinaries.com>
2017-03-30 12:16:50 +0000

    Documented motor pinouts

---
+++ motor.py
@@ -0,0 +1,28 @@
+# motor.py  30 March 2017  D.J.Whale
+#
+# Example of driving 4 motors from a Raspberry Pi
+# Using 4tronix (camjam) motor controller H bridge board
+
+# MOTOR B - normal controller
+BACK_RIGHT_B = 7
+BACK_RIGHT_F = 8
+
+# MOTOR A - normal controller
+FRONT_RIGHT_B = 9
+FRONT_RIGHT_F = 10
+
+# MOTOR B - extra controller
+BACK_LEFT_B  = 22 # 7
+BACK_LEFT_F  = 23 # 8
+
+# MOTOR A - extra controller
+FRONT_LEFT_B  = 24 # 9
+FRONT_LEFT_F  = 25 # 10
+
+
+
+
+
+
+
+
